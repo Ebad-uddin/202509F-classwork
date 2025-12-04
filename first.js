d1255e32b689c2a1401a4b5b90b63f905906a340
ajmal4574 <ebad0300@gmail.com>
2025-12-04 19:01:57 +0500

    updated work

---
+++ first.js
@@ -0,0 +1,20 @@
+
+
+
+// javascript is a programming language used to add the logics in website
+
+// types (internal and external)
+
+/* 
+internal js
+
+1) within html file and between <script> tag you can implement javascript.
+
+external js
+
+1) An external file will be created as like css and include this file in your html file like (<script src="jsfile.js">) above the body end tag.
+
+*/
+
+
+// syntax
